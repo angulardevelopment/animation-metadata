cf0c3988b94bb16d306e0d5dc2910e82852a7a74
Gupta, Sunny <sunnyvales789@gmail.com>
2024-12-31 01:06:14 +0530

    update

--- src/app/parent-child-animation/parent-child-animation.component.ts
+++ src/app/parent-child-animation/parent-child-animation.component.ts
@@ -6,11 +6,13 @@ import { trigger, transition, style, animate } from '@angular/animations';
   templateUrl: './parent-child-animation.component.html',
   styleUrls: ['./parent-child-animation.component.scss'],
   animations: [
+    // The `fadeInOut` trigger defines animations for entering and leaving. The child element fades in when it appears and fades out when it leaves.
     trigger('fadeInOut', [
       transition(':enter', [
         style({ opacity: 0 }),
         animate('1s', style({ opacity: 1 })),
       ]),
+      // When you click the "Toggle Child Element" button, the child will fade out first. If you click "Remove Parent Element," the parent will disappear immediately, but only after the child has completed its fade-out animation.
       transition(':leave', [
         animate('1s', style({ opacity: 0 })),
       ]),
@@ -26,6 +28,7 @@ export class ParentChildAnimationComponent implements OnInit {
   showParent = true;
   showChild = false;
 
+  // The `toggleChild` method controls the visibility of the child element. The `removeParent` method sets `showParent` to false, which will remove the parent element.
   toggleChild() {
     this.showChild = !this.showChild;
   }
